24c1dac8df97e21e77fd3af443552ec135f5dba2
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-11-29 17:06:38 +0100

    Update load-tailwind.js

--- www/load-tailwind.js
+++ www/load-tailwind.js
@@ -1,12 +1,39 @@
 (function () {
-  try {
-    var tailwindStyle = document.getElementById("/src/index.css:-css");
+  var cssId = "/src/index.css:-css";
+  function activateTailwind() {
+    var tailwindStyle = document.getElementById(cssId);
     if (tailwindStyle) {
-      tailwindStyle.setAttribute("type", "text/tailwind");
+      console.log("updating tailwind css type");
+      tailwindStyle.setAttribute("type", "text/tailwindcss");
     }
 
+    // delete old tailwind
+    var oldTailwind = document.getElementById("tailwindcss-script");
+    if (oldTailwind) {
+      console.log("deleting old tailwind");
+      oldTailwind.remove();
+    }
+
+    console.log("loading tailwind script");
     var script = document.createElement("script");
+    script.id = "tailwindcss-script";
     script.src = "https://cdn.tailwindcss.com";
     document.head.appendChild(script);
+  }
+
+  try {
+    var config = { attributes: true, childList: true, subtree: true };
+
+    function callback(mutationList, observer) {
+      var tailwindStyle = document.head.getElementById(cssId);
+      console.log("head change");
+      if (tailwindStyle && tailwindStyle.getAttribute("type") === "text/css") {
+        console.log("tailwind not activated");
+        activateTailwind();
+      }
+    }
+
+    var observer = new MutationObserver(callback);
+    observer.observe(document.head, config);
   } catch {}
 })();
