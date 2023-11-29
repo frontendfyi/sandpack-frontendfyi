acd120ba8c9cafd786f275bc470cc5c44bdc9986
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-11-29 16:51:34 +0100

    Update load-tailwind.js

--- www/load-tailwind.js
+++ www/load-tailwind.js
@@ -1,14 +1,12 @@
 (function () {
   try {
-    setTimeout(function(){
-      var tailwindStyle = document.getElementById("/src/index.css:-css");
-      if (taildwindStyle) {
-        tailwindStyle.setAttribute("type", "text/tailwind");
-      }
-    
-      var script = document.createElement("script");
-      script.src = "https://cdn.tailwindcss.com";
-      document.head.appendChild(script);
-    }, 200);
+    var tailwindStyle = document.getElementById("/src/index.css:-css");
+    if (tailwindStyle) {
+      tailwindStyle.setAttribute("type", "text/tailwind");
+    }
+
+    var script = document.createElement("script");
+    script.src = "https://cdn.tailwindcss.com";
+    document.head.appendChild(script);
   } catch {}
 })();
